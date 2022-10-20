0398fd9d53bc337093eb956670130d4de921b0df
Tw1nM4x <madexn@mail.ru>
2022-10-20 16:11:32 +1000

    first commit
    Первый коммит
    Не работает программа для запуска тестов
    Анализатор считает большинство лексем, но не вычисляет их значения

---
+++ Compiler/Tester.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Globalization;
+using System.Reflection.PortableExecutable;
+
+namespace Compiler
+{ 
+    class Tester
+    {
+        static void Main()
+        {
+            int numberTest = 1;
+            string pathIn = "../../../" + $"00{numberTest}_input.txt";
+            string pathOut = "../../../" + $"00{numberTest}_out.txt";
+            Console.WriteLine($"Нажмите на Enter для запуска теста {numberTest}");
+            string? input = Console.ReadLine();
+            if(input == null)
+            {
+                Compiler.Main(pathIn, pathOut);
+            }
+        }
+    }
+}
